c9ad0a5d8cf22cccf6ce883feb7d2259492854b8
mp8671 <96391626+MaksimPikovskiy@users.noreply.github.com>
2022-04-13 00:01:51 -0400

    Create WeightedInstance.py

---
+++ learners/data_structs/WeightedInstance.py
@@ -0,0 +1,62 @@
+class WeightedInstance:
+    """
+    A class used to represent a Weighted Instance (or sample) for AdaBoost.
+
+    Attributes
+    ------------
+    data : array of Examples
+        Examples for this Weighted Instance
+    sum : int
+        the weight of this Weighted Instance
+    normalized_sum : float
+        the normalized weight of this Weighted Instance
+
+    Methods
+    ------------
+    normalize()
+        normalizes the sum/weight of this Weighted Instance
+    change_weight(i, new_weight)
+        changes the weight of data at index i and adjusts the sum to that change
+    """
+    data = None
+    sum = 0
+    normalized_sum = 0
+
+    def __init__(self, examples):
+        """
+        Initializes Weighted Instance.
+
+        :param examples: the examples to be contained in this Weighted Instance.
+        """
+        self.data = examples
+
+        for example in self.data:
+            example.weight = 1
+            self.sum += example.weight
+
+        self.normalized_sum = self.sum
+
+    def normalize(self):
+        """
+        Normalizes the weight of this Weighted Instance.
+
+        :return: none
+        """
+        normalization_constant = self.normalized_sum / self.sum
+        self.sum = 0
+
+        for example in self.data:
+            example.weight *= normalization_constant
+            self.sum += example.weight
+
+    def change_weight(self, i, new_weight):
+        """
+        Changes the weight of the data at index i and adjusts the sum to that change.
+
+        :param i: the index of the data in the Weighted Instance to change the weight for.
+        :param new_weight: the new weight for the data at index i
+        :return: none
+        """
+        self.sum -= self.data[i].weight
+        self.data[i].weight = new_weight
+        self.sum += new_weight
